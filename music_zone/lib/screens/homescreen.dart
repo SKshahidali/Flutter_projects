ca57a5f0fe26f2316071ff1952a20095d6bbae47
Sk Shahid Ali <87206489+SKshahidali@users.noreply.github.com>
2023-11-01 12:13:59 +0530

    Update homescreen.dart

--- music_zone/lib/screens/homescreen.dart
+++ music_zone/lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_zone/widgets/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(backgroundColor: grey,
+      appBar:AppBar(  
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.window_rounded,color: yellow,size: 30,),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 8),
+            child: CircleAvatar(
+              backgroundImage:NetworkImage('https://img.freepik.com/premium-photo/cartoon-character-with-glasses-red-shirt-that-says-i-m-boy_771335-49728.jpg?w=740'),
+              backgroundColor: Colors.transparent,
+              ),
+          ),
+        ],
+      ),
+    );
   }
 }
